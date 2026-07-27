96d5eecf3c8cfbb70ea2bdc2e18e0ee3c70d4feb
iki1uc <entertrainer@web.de>
2026-07-27 12:01:55 +0200

    Create Orb.js

---
+++ Orb.js
@@ -0,0 +1,17 @@
+export const HHOrbit_Pyramid = {
+  S1: { rows: 8, cols: 4 },
+  S2: { rows: 4, cols: 8 },
+  S3: { rows: 16, cols: 2 },
+  S4: { rows: 2, cols: 16 },
+
+  mesh: Pyramid128Mesh,
+
+  map() {
+    return {
+      O1: this.S1,
+      O2: this.S2,
+      O3: this.S3,
+      O4: this.S4
+    };
+  }
+};
