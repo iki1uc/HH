8bebfaffd3b8e5efe89125e441fa51cf2929eb80
iki1uc <entertrainer@web.de>
2026-07-27 11:34:54 +0200

    Update HYPER.core.js

--- HYPER.core.js
+++ HYPER.core.js
@@ -1,13 +1,23 @@
-export const HYPER = {
+import { HHPyramid128 } from "./HH-Orbit.js";
 
-    activate(hh) {
-        return {
-            HYPER: "ACTIVE",
-            MODE: "HYPER-REAL",
-            LEVEL: "6D",
-            ORBIT: hh.orbit,
-            VEC: hh.vec,
-            STATE: "PARALLEL-SUN"
-        };
-    }
+export const HYPERcore = {
+  load() {
+    const orbitMap = HHPyramid128.mapToOrbit();
+    return this.computeOPtA(orbitMap);
+  },
+
+  computeOPtA(orbitMap) {
+    const TMP = (side) => side.rows * side.cols; // dein TMP-Operator
+
+    const core = {
+      C1: TMP(orbitMap.O1),
+      C2: TMP(orbitMap.O2),
+      C3: TMP(orbitMap.O3),
+      C4: TMP(orbitMap.O4)
+    };
+
+    core.OPtA = core.C1 + core.C2 + core.C3 + core.C4;
+
+    return core;
+  }
 };
