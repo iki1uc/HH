eb4998aed7aa5e41c38eaac921446eb8ea525b9a
iki1uc <entertrainer@web.de>
2026-07-27 10:30:53 +0200

    Add HH_VEC module with compute function

---
+++ HH.VEC.js
@@ -0,0 +1,19 @@
+export const HH_VEC = {
+
+    compute(axiom) {
+        return {
+            VEC: [
+                axiom.A1,
+                axiom.A2,
+                axiom.A3,
+                axiom.A4,
+                axiom.A5,
+                axiom.A6,
+                axiom.A7
+            ],
+            orbit: "HH-ORBIT-7D",
+            axiom: 7,
+            mode: "Parallel-Real"
+        };
+    }
+};
